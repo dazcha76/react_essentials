f9c80107472276873bed9bbfda6f18d3773c13bf
Danika <dazcha@hotmail.com>
2026-07-19 13:26:07 +0100

    added concept component

---
+++ src/components/core-concept/core-concept.tsx
@@ -0,0 +1,19 @@
+type CoreConceptProps = {
+  title: string;
+  description: string;
+  image: string;
+};
+
+export default function CoreConcept({
+  title,
+  description,
+  image,
+}: CoreConceptProps) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,22 @@
-function Header() {
+import reactImg from '../../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+export default function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
   );
 }
-
-export default Header;
